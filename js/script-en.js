7f9b5f6ebadb32f09f65c82558139e5d7b97e366
Keven Imbeault <xkeke111x@gmail.com>
2017-09-05 15:42:30 -0400

    Ajout du script pour la version anglaise du site et corrections de bugs

--- js/script-en.js
+++ js/script-en.js
@@ -3,15 +3,16 @@ $('.index_links').on('click', function(){
 });
 
 $("#darkmode").click(function(){
-  if($("#darkmode").html() === "Mode sombre") {
+  if($("#darkmode").html() === "Dark mode") {
       $("body").css("background", "#161616");
       $(".panel").css("color", "white");
       $(".panel").css("background-color", "#161616");
-      $("#darkmode").html("Mode normal");
-  } else if($("#darkmode").html() === "Mode normal") {
+      $("i").css("color", "white");
+      $("#darkmode").html("Light mode");
+  } else if($("#darkmode").html() === "Light mode") {
       $(".panel").css("color", "black");
       $(".panel").css("background-color", "white");
       $("body").css("background", "-webkit-linear-gradient(top,  #00c1fc 0%,#007fad 100%)");
-      $("#darkmode").html("Mode sombre");
+      $("#darkmode").html("Dark mode");
   }
 });
